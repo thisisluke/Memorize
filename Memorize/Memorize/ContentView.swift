fb11208d20c6fc9dc54b0f532cb8421993a74ee3
thisisluke <luke.liu.au@outlook.com>
2020-08-18 22:22:17 +1000

    project init

---
+++ Memorize/Memorize/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  Memorize
+//
+//  Created by luke2015 on 18/8/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!").padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
